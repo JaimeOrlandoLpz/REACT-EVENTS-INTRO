70d1a067a7a83fc61475db027b4357bf1cf02388
Jaime Orlando Lopez Ramos <30807749+JaimeOrlandoLpz@users.noreply.github.com>
2021-08-21 22:27:26 -0500

    Mouse Over and Out Events

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [headingText, setHeadingText] = useState("Hello, World!");
+  const [btnClassName, setBtnClassName] = useState("whiteBg");
+
+  function handleClick() {
+    setHeadingText("Submitted");
+  }
+
+  function handleMouseOver() {
+    setBtnClassName("blackBg");
+  }
+
+  function handleMouseOut() {
+    setBtnClassName("whiteBg");
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>{headingText}</h1>
       <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <button
+        onClick={handleClick}
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+        className={btnClassName}
+      >
+        Submit
+      </button>
     </div>
   );
 }
